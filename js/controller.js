d67535776e3bda3ecc996913d843d67e0a66d5a8
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-26 20:30:10 +0100

    pullling data from inside controller using $scope and ng-repeat

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,22 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('ListController', function() {
-
-  
-});
+myApp.controller('ListController', ['$scope', function($scope) {
+  $scope.artists = [
+    {
+      "name": "Stefan",
+     "company": "Acer Corp"
+   },
+     {
+       "name": "Mike",
+       "company": "IBM"
+     },
+     {
+       "name": "Tony",
+       "company": "Hewlett Packard"
+     },
+     {
+       "name":"Curtis",
+       "company": "Nasa"
+     }
+  ]
+}]);
