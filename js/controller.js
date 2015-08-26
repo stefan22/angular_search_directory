73c4f9e6f4820d121d24d4c9f70f6e8802a5594f
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-26 23:42:23 +0100

    initializes select box name inside of controller

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,7 @@ var listController = angular.module('ListController', []);
 listController.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
-  
+    $scope.order = 'name';
+
   });
 }]);
