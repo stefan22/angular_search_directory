5bfbaeb456688d41892369e34a677426bfe5d832
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-27 16:14:42 +0100

    adds route provider using config inside of app controller

--- js/app.js
+++ js/app.js
@@ -2,3 +2,14 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'listControllers'
 ]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/list', {
+    templateUrl: 'partials/list.html',
+    controller: 'ListController'
+  }).
+  otherwise({
+    redirectTo: 'list'
+  });
+}]);
